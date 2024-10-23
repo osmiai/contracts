0e1d3ae32d2edae3822ac076bc83192e3805f1c1
Stephen Nichols <snichols@gala.games>
2024-10-23 14:58:18 -0500

    initial sepolia deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox"
 import "@openzeppelin/hardhat-upgrades"
 
 const ALCHEMY_URL_SEPOLIA = vars.get("ALCHEMY_URL_SEPOLIA")
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY")
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -10,6 +11,7 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: ALCHEMY_URL_SEPOLIA,
+      accounts: [SEPOLIA_PRIVATE_KEY],
     }
   }
 }
